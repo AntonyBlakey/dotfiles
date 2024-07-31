81d43643c464469eb27095c9b4fe52eaff15b50c
Antony Blakey <antony.blakey@gmail.com>
2024-07-31 22:04:34 +1000

    Update setup-homebrew.sh to simplify

--- bin/setup-homebrew.sh
+++ bin/setup-homebrew.sh
@@ -1,28 +1,5 @@
 #!/usr/bin/env zsh
 
-for p in git git-extras git-lfs curl wget xz gawk jq gpg htop ; do
+for p in jq fzf ; do
     brew install $p
 done
-
-# Soops
-# if [[ "$OSTYPE" == darwin* ]] ; then
-# for p in sshuttle ; do
-#     brew install $p
-# done
-#fi
-
-brew tap microsoft/git
-brew install --cask git-credential-manager
-
-# if [[ "$OSTYPE" == darwin* ]] ; then
-# brew install --cask 1password
-# brew install --cask notion
-# brew install --cask arc
-# brew install --cask warp
-# brew install --cask docker
-# brew install --cask visual-studio-code
-# brew install --cask skype
-# brew install --cask slack 
-# brew install --cask telegram 
-# brew install --cask racket
-# fi
